3da58989bfb6748f179e7d9b8a111a7c947b32e5
Nathan Henderson <neh45@byu.edu>
2026-09-11 18:05:23 -0600

    Behold my own debug board. And behold I setup the board wrong.

--- shared/src/test/java/passoff/chess/NatesTests.java
+++ shared/src/test/java/passoff/chess/NatesTests.java
@@ -1,9 +1,13 @@
 package passoff.chess;
 
 import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
 import chess.ChessPosition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.text.Position;
 
 public class NatesTests {
     @Test
@@ -14,9 +18,50 @@ public class NatesTests {
         System.out.println(board.getPiece(new ChessPosition(1,1)).getTeamColor());
     }
 
+    public String convertPieceToString(ChessPiece piece){
+        String team;
+        String pieceType;
+
+        if (piece == null){
+            return " ";
+        }
+
+        if (piece.getTeamColor() == ChessGame.TeamColor.BLACK){
+            team = "b";
+        }else{
+            team = "w";
+        }
+
+        pieceType = switch (piece.getPieceType()){
+            case ChessPiece.PieceType.BISHOP -> "b";
+            case ChessPiece.PieceType.KING -> "k";
+            case ChessPiece.PieceType.PAWN -> "p";
+            case ChessPiece.PieceType.QUEEN -> "q";
+            case ChessPiece.PieceType.KNIGHT -> "n";
+            case ChessPiece.PieceType.ROOK -> "r";
+
+            default -> "blank";
+        };
+
+        return team + pieceType;
+    }
+
     @Test
     @DisplayName("present board")
     public void presentBoard(){
+        ChessBoard board = new ChessBoard();
+
+        for (int i = 0; i < 8; i++){
+            System.out.println("_________________");
+            String boardLine = "";
+            for (int a = 0; a < 8; a++){
+                ChessPiece pieceFound = board.getPiece(new ChessPosition(a + 1,i + 1));
+                String pieceValue = convertPieceToString(pieceFound);
+
+                boardLine = boardLine + pieceValue + "|";
+            }
+            System.out.println(boardLine);
+        }
 
     }
 }
